c8a58a45e3d2ba077871d5c8e9cb2557f8a5be12
Marina <marinabrito.ma@gmail.com>
2021-03-19 13:39:40 +0000

    update

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-const FULL_DASH_ARRAY = 283;
+const startingMinutes = 10;
+let time = startingMinutes * 60;
+
+const appEl = document.getElementById('app');
+
+setInterval(updateApp, 1000);
+
+function updateApp(){
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+
+    appEl.innerHTML = `${minutes}:${seconds}`;
+    time--;
+}
